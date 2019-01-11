36a4400b30c719617a5629305856d8948710c76f
Ken Domino <kenneth.domino@domemtech.com>
2019-01-10 20:58:24 -0500

    Most changes for getting a simple cuda program working. However, my trick to use struct as an alias (typedef) does not work--passing a struct passes the address of the struct, the the value. I thought it would work because structs are value types. But the compiler treats them as pseudo classes. Templates will need to be fixed for typedefs.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,8 +1,13 @@
-﻿namespace test
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace test
 {
     using System.Text;
     using System;
     using Cuda;
+    using System.Linq;
 
     class Test
     {
@@ -18,7 +23,7 @@
             }
         }
 
-        public static void Gpu()
+        public static unsafe void Gpu()
         {
             Cuda.CUcontext ctx = new CUcontext(IntPtr.Zero);
             try
@@ -44,6 +49,66 @@
 
                     System.Console.WriteLine("--------");
                     System.Console.WriteLine(Helper.OutProps(props, n));
+                }
+
+                {
+                    res = Cuda.Functions.cuDeviceGet(out CUdevice device, 0);
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    res = Cuda.Functions.cuCtxCreate_v2(out CUcontext cuContext, 0, device);
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    string path = Assembly.GetAssembly(typeof(Program)).Location;
+                    path = Path.GetDirectoryName(path);
+                    path = Path.GetFullPath(path + @"\..\..\..\..");
+                    path = path + @"\cuda\x64\Debug\vector-sum.ptx";
+                    StreamReader sr = new StreamReader(path);
+                    String ptx = sr.ReadToEnd();
+                    IntPtr ptr = Marshal.StringToHGlobalAnsi(ptx);
+                    res = Cuda.Functions.cuModuleLoadData(out CUmodule module, ptr);
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    res = Cuda.Functions.cuModuleGetFunction(out CUfunction helloWorld, module, "VectorSumParallel");
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    int n = 3;
+                    int[] a = Enumerable.Range(1, 3).Select(v => v * 3).ToArray();
+                    int[] b = Enumerable.Range(1, 3).Select(v => v * 2).ToArray();
+                    int[] c = new int[3];
+                    res = Cuda.Functions.cuMemAlloc_v2(out CUdeviceptr d_a, n * sizeof(int));
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    res = Cuda.Functions.cuMemAlloc_v2(out CUdeviceptr d_b, n * sizeof(int));
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    res = Cuda.Functions.cuMemAlloc_v2(out CUdeviceptr d_c, n * sizeof(int));
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    var ha = GCHandle.Alloc(a, GCHandleType.Pinned);
+                    var hb = GCHandle.Alloc(b, GCHandleType.Pinned);
+                    var hc = GCHandle.Alloc(c, GCHandleType.Pinned);
+                    var pa = ha.AddrOfPinnedObject();
+                    var pb = hb.AddrOfPinnedObject();
+                    var pc = hc.AddrOfPinnedObject();
+                    res = Cuda.Functions.cuMemcpyHtoD_v2(d_a, pa, sizeof(int) * n);
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    res = Cuda.Functions.cuMemcpyHtoD_v2(d_b, pb, sizeof(int) * n);
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    IntPtr[] x = new IntPtr[] { (IntPtr)d_a.Value, (IntPtr)d_b.Value, (IntPtr)d_c.Value, (IntPtr)n };
+                    GCHandle handle2 = GCHandle.Alloc(x, GCHandleType.Pinned);
+                    IntPtr pointer2 = IntPtr.Zero;
+                    pointer2 = handle2.AddrOfPinnedObject();
+                    IntPtr[] kp = new IntPtr[] { pointer2 };
+                    fixed (IntPtr* kernelParams = kp)
+                    {
+                        res = Cuda.Functions.cuLaunchKernel(
+                            helloWorld,
+                            1, 1, 1, // grid has one block.
+                            (uint)n, 1, 1, // block has 11 threads.
+                            0, // no shared memory
+                            default(CUstream),
+                            (IntPtr)kernelParams,
+                            (IntPtr)IntPtr.Zero
+                        );
+                    }
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    res = Cuda.Functions.cuMemcpyDtoH_v2(hc.AddrOfPinnedObject(), d_c, n * sizeof(int));
+                    if (res.Value != cudaError_enum.CUDA_SUCCESS) throw new Exception();
+                    Cuda.Functions.cuCtxDestroy_v2(cuContext);
+                    System.Console.WriteLine(String.Join(" ",c));
                 }
             }
             finally
